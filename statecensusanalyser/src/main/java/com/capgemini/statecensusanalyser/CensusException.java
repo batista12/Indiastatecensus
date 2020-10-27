841bf6d6b144bc56a79cdb82f053b8070f4c2f82
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-27 01:59:01 -0700

    [Manasi]UCR1 dry principle was violated for state census

--- statecensusanalyser/src/main/java/com/capgemini/statecensusanalyser/CensusException.java
+++ statecensusanalyser/src/main/java/com/capgemini/statecensusanalyser/CensusException.java
@@ -3,18 +3,17 @@ package com.capgemini.statecensusanalyser;
  * @author ASUS
  *
  */
-public class CensusAnalyserException extends Exception{
-    enum CensusExceptionType{
-        NO_SUCH_FILE, INCORRECT_DATA_ISSUE, SOME_OTHER_IO_EXCEPTION, DELIMITER_ISSUE,  NO_SUCH_CLASS
-    }
-    CensusExceptionType type;
-    private String message;
+enum ExceptionType {
+	STATE_CENSUS_FILE_PROBLEM, INCORRECT_TYPE, SOME_OTHER_ERRORS
+}
 
-    public CensusAnalyserException() {
-    }
+@SuppressWarnings("serial")
+public class CensusException extends Exception {
+	public static final Object[] STATE_CENSUS_FILE_PROBLEM = null;
+	public static final Object[] INCORRECT_TYPE = null;
+	public Object[] exceptionType;
 
-    public CensusAnalyserException(CensusExceptionType type, String message) {
-        super(message);
-        this.type = type;
-    }
+	public CustomStateCensusAnalyserException(String message, ExceptionType exceptionType) {
+		super(exceptionType.toString());
+	}
 }
