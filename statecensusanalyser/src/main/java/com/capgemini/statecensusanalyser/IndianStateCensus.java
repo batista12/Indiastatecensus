442f04681c2168e35091353528dd278f3f0d8952
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-27 10:29:46 -0700

    [Manasi]IndianstateCensus Analsyer is created

---
+++ statecensusanalyser/src/main/java/com/capgemini/statecensusanalyser/IndianStateCensus.java
@@ -0,0 +1,13 @@
+package com.capgemini.statecensusanalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class IndianStateCensus 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ statecensusanalyser/src/test/java/com/capgemini/statecensusanalyser/IndianStateCensusTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.statecensusanalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class IndianStateCensusTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
