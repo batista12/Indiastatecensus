01132b88775ba199e1be900634bc612257f34dad
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-26 23:47:18 -0700

    [Manasi]State Code Test file is created

--- statecensusanalyser/src/test/java/com/capgemini/statecensusanalyser/IndianStateCensusTest.java
+++ statecensusanalyser/src/test/java/com/capgemini/statecensusanalyser/IndianStateCensusTest.java
@@ -1,20 +1,78 @@
 package com.capgemini.statecensusanalyser;
-
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import com.opencsv.bean.MappingStrategy;
 
-/**
- * Unit test for simple App.
- */
-public class IndianStateCensusTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class CensusAnalyserTest {
+	private final String STATE_CENSUS_CSV_FILE = "./src/main/resources/StateCensusCSVData.csv";
+	private final String INCORRECT_STATE_CENSUS_CSV_FILE = "./src/main/resources/_StateCensusCSVData.csv";
+	private final String INCORRECT_HEADER_STATE_CENSUS_CSV_FILE = "./src/main/resources/IndianStateCensusDataIncorrectHeader.csv";
+
+	@Test
+	public void givenTheStatesCensusCSVFile_WhenRead_NoOfRecordsShouldMatch()
+			throws CustomStateCensusAnalyserException, CustomFileIOException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		MappingStrategy<CSVStateCensus> mappingStrategy = new HeaderColumnNameMappingStrategy<CSVStateCensus>();
+		mappingStrategy.setType(CSVStateCensus.class);
+		int numOfRecords = stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_CSV_FILE, mappingStrategy,
+				CSVStateCensus.class, ',');
+		Assert.assertEquals(6, numOfRecords);
+	}
+
+	@Test
+	public void givenIncorrectCSVFile_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			MappingStrategy<CSVStateCensus> mappingStrategy = new HeaderColumnNameMappingStrategy<CSVStateCensus>();
+			mappingStrategy.setType(CSVStateCensus.class);
+			stateCensusAnalyser.loadStateCensusData(INCORRECT_STATE_CENSUS_CSV_FILE, mappingStrategy,
+					CSVStateCensus.class, ',');
+		} catch (CustomStateCensusAnalyserException | CustomFileIOException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.STATE_CENSUS_FILE_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenIncorrectCSVType_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_CSV_FILE, null, null, ',');
+		} catch (CustomStateCensusAnalyserException | CustomFileIOException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.STATE_CENSUS_PARSE_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenCorrectCSVFileIncorrectDelimiter_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			MappingStrategy<CSVStateCensus> mappingStrategy = new HeaderColumnNameMappingStrategy<CSVStateCensus>();
+			mappingStrategy.setType(CSVStateCensus.class);
+			stateCensusAnalyser.loadStateCensusData(STATE_CENSUS_CSV_FILE, mappingStrategy, CSVStateCensus.class, '|');
+		} catch (CustomStateCensusAnalyserException | CustomFileIOException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.STATE_CENSUS_HEADER_OR_DELIMITER_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenCorrectCSVFileIncorrectHeader_ShouldReturnCustomException() {
+		String exceptionMessage = null;
+		try {
+			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+			MappingStrategy<CSVStateCensus> mappingStrategy = new HeaderColumnNameMappingStrategy<CSVStateCensus>();
+			mappingStrategy.setType(CSVStateCensus.class);
+			stateCensusAnalyser.loadStateCensusData(INCORRECT_HEADER_STATE_CENSUS_CSV_FILE, mappingStrategy,
+					CSVStateCensus.class, ',');
+		} catch (CustomStateCensusAnalyserException | CustomFileIOException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.STATE_CENSUS_HEADER_OR_DELIMITER_PROBLEM.toString(), exceptionMessage);
+	}
 }
